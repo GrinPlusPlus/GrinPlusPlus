5610177bda28a0820016a2f881586ada6cc6a8c8
David Burkett <davidburkett38@gmail.com>
2019-09-13 08:00:02 -0400

    Adding placeholders for future NodeClients.

---
+++ Server/Node/NodeClients/RPCNodeClient.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <Wallet/NodeClient.h>
+
+class RPCNodeClient : public INodeClient
+{
+	// TODO: Implement using https://github.com/rpclib/rpclib
+};
---
+++ Server/Node/NodeClients/RustNodeClient.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <Wallet/NodeClient.h>
+
+class RustNodeClient : public INodeClient
+{
+	// TODO: Implement Rest calls to core Rust node
+};

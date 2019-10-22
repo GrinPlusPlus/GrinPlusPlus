f8ed96135414b756902f6b81f3969fbb4c6b4249
David Tavarez <davidftavarez@gmail.com>
2019-10-22 11:34:46 +0200

    Update StringUtil.h

--- include/Common/Util/StringUtil.h
+++ include/Common/Util/StringUtil.h
@@ -13,23 +13,12 @@
 class StringUtil
 {
 public:
-	template<class T>
-	static decltype(auto) convert_for_snprintf(T const& x)
-	{
-		return x;
-	}
-
-	static decltype(auto) convert_for_snprintf(std::string const& x)
-	{
-		return x.c_str();
-	}
-	
 	template<typename ... Args>
 	static std::string Format(const std::string& format, Args ... args)
 	{
 		size_t size = std::snprintf(nullptr, 0, format.c_str(), convert_for_snprintf(args) ...) + 1; // Extra space for '\0'
 		std::unique_ptr<char[]> buf(new char[size]);
-		std::snprintf(buf.get(), size, format.c_str(), StringUtil::convert_for_snprintf(args) ...);
+		std::snprintf(buf.get(), size, format.c_str(), convert_for_snprintf(args) ...);
 		return std::string(buf.get(), buf.get() + size - 1); // We don't want the '\0' inside
 	}
 
@@ -111,5 +100,16 @@ private:
 		s.erase(std::find_if(s.rbegin(), s.rend(), [](int ch) {
 			return !std::isspace(ch) && ch != '\r' && ch != '\n';
 			}).base(), s.end());
+	}
+	
+	template<class T>
+	static decltype(auto) convert_for_snprintf(T const& x)
+	{
+		return x;
+	}
+
+	static decltype(auto) convert_for_snprintf(std::string const& x)
+	{
+		return x.c_str();
 	}
 };

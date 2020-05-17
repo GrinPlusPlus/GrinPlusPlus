b76e219a079194f90c8815437a4d81b8371e6e05
David Burkett <davidburkett38@gmail.com>
2020-05-16 23:41:52 -0400

    Boost sucks

--- src/Server/opt.h
+++ src/Server/opt.h
@@ -2,7 +2,7 @@
 
 #include <Config/EnvironmentType.h>
 #include <Net/SocketAddress.h>
-#include <boost/program_options.hpp>
+#include <klib/ketopt.h>
 #include <iostream>
 #include <optional>
 
@@ -15,43 +15,48 @@ struct Options
 	std::optional<SocketAddress> shared_node;
 };
 
-static boost::program_options::options_description GetDescription()
-{
-	namespace po = boost::program_options;
-
-	po::options_description description("Usage: GrinPP [options]");
-
-	// TODO: override config file path
-	description.add_options()
-		("help,h", "Display this help message")
-		("floonet", "Use floonet chain")
-		("headless", po::value<std::string>(), "Run headless app with no console output")
-		("node-only", "Run only a node, not a wallet")
-		("shared-node", po::value<std::string>(), "Runs only a wallet, and uses the node at a given IP address and port (###.###.###.###:####)");
-
-	return description;
-}
+static ko_longopt_t longopts[] = {
+	{ "help", ko_no_argument, 301 },
+	{ "floonet", ko_no_argument, 302 },
+	{ "headless", ko_no_argument, 303 },
+	{ "no-wallet", ko_no_argument, 304 },
+	{ "shared-node", ko_required_argument, 305 },
+	{ NULL, 0, 0 }
+};
 
 static Options ParseOptions(int argc, char* argv[])
 {
-	namespace po = boost::program_options;
-
-	auto description = GetDescription();
-
-	po::variables_map vm;
-	po::store(po::command_line_parser(argc, argv).options(description).run(), vm);
-	po::notify(vm);
-
-	return Options{
-		vm.count("help") > 0,
-		vm.count("headless") > 0 || vm.count("shared-node") > 0,
-		vm.count("floonet") > 0 ? EEnvironmentType::FLOONET : EEnvironmentType::MAINNET,
-		vm.count("node-only") == 0,
-		vm.count("shared-node") > 0 ? std::make_optional<SocketAddress>(SocketAddress::Parse(vm.at("shared-node").as<std::string>())) : std::nullopt
+	Options options{
+		false,
+		false,
+		EEnvironmentType::MAINNET,
+		true,
+		std::nullopt
 	};
+
+	ketopt_t opt = KETOPT_INIT;
+	int c;
+	while ((c = ketopt(&opt, argc, argv, 1, "xy:", longopts)) >= 0) {
+		if (c == 301) options.help = true;
+		else if (c == 302) options.environment = EEnvironmentType::FLOONET;
+		else if (c == 303) options.headless = true;
+		else if (c == 304) options.include_wallet = false;
+		else if (c == 305)
+		{
+			options.shared_node = std::make_optional<SocketAddress>(SocketAddress::Parse(opt.arg));
+			options.headless = true;
+		}
+	}
+
+	return options;
 }
 
 static void PrintHelp()
 {
-	std::cout << GetDescription() << std::endl;
+	std::cout << "Usage: GrinPP [options]" << std::endl;
+	std::cout << "--help" << "\t" << "Display this help message" << std::endl;
+	std::cout << "--floonet" << "\t" << "Use floonet chain" << std::endl;
+	std::cout << "--headless" << "\t" << "Run headless app with no console output" << std::endl;
+	std::cout << "--no-wallet" << "\t" << "Run only a node, not a wallet" << std::endl;
+	std::cout << "--shared-node [IP:port]" << "\t" << "Uses the node at a given IP address and port (###.###.###.###:####)" << std::endl;
 }

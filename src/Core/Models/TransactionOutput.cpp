7877be82196d699b382b9402d3dc7c9d301ab987
David Burkett <davidburkett38@gmail.com>
2020-07-29 10:07:54 -0400

    Output hashing fix

--- src/Core/Models/TransactionOutput.cpp
+++ src/Core/Models/TransactionOutput.cpp
@@ -59,7 +59,11 @@ const Hash& TransactionOutput::GetHash() const
 {
 	if (m_hash == Hash{}) {
 		Serializer serializer;
-		Serialize(serializer);
+		// Serialize OutputFeatures
+		serializer.Append<uint8_t>((uint8_t)m_features);
+
+		// Serialize Commitment
+		m_commitment.Serialize(serializer);
 		m_hash = Crypto::Blake2b(serializer.GetBytes());
 	}
 

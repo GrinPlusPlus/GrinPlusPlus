a0b5430a11235ada4fa419ac8510a8d4602d85bd
David Burkett <davidburkett38@gmail.com>
2020-10-23 09:57:58 -0400

    Fixing input sorting during tx building

--- include/Core/Models/TransactionInput.h
+++ include/Core/Models/TransactionInput.h
@@ -24,7 +24,7 @@ public:
 	//
 	TransactionInput(const EOutputFeatures features, Commitment&& commitment);
 	TransactionInput(const EOutputFeatures features, const Commitment& commitment)
-		: m_features(features), m_commitment(commitment) { }
+		: TransactionInput(features, Commitment(commitment)) {  }
 	TransactionInput(const TransactionInput& transactionInput) = default;
 	TransactionInput(TransactionInput&& transactionInput) noexcept = default;
 	TransactionInput() = default;

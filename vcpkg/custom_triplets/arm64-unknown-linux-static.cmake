05f91d8e2a59fd71df3447519307b0e0e0b3155b
David Tavarez <david@punksec.com>
2020-11-09 20:57:32 +0100

    improve arm64 linux code

---
+++ vcpkg/custom_triplets/arm64-unknown-linux-static.cmake
@@ -0,0 +1,8 @@
+set(VCPKG_TARGET_ARCHITECTURE arm64)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE static)
+
+set(VCPKG_CMAKE_SYSTEM_NAME Linux)
+
+set(CMAKE_CXX_COMPILER "aarch64-unknown-linux-gnueabi-g++")
+set(CMAKE_C_COMPILER "aarch64-unknown-linux-gnueabi-gcc")

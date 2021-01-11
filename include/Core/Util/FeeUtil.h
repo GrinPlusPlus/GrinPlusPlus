25b20d9f73db1e1f029e8ddecb0cef7a975bca0b
David Burkett <davidburkett38@gmail.com>
2021-01-11 05:22:56 -0500

    Updating fee weight calculation

--- include/Core/Util/FeeUtil.h
+++ include/Core/Util/FeeUtil.h
@@ -7,6 +7,6 @@ class FeeUtil
 public:
 	static uint64_t CalculateFee(const uint64_t feeBase, const int64_t numInputs, const int64_t numOutputs, const int64_t numKernels)
 	{
-		return feeBase * Consensus::CalculateWeightV4(numInputs, numOutputs, numKernels);
+		return feeBase * Consensus::CalculateWeightV5(numInputs, numOutputs, numKernels);
 	}
 };

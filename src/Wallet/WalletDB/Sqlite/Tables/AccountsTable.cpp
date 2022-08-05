8b10f21c49c103f8fd6872b97034b9d1fa05fcbe
David Tavarez <337107+davidtavarez@users.noreply.github.com>
2022-08-05 12:04:37 +0200

    fixing account table creation

--- src/Wallet/WalletDB/Sqlite/Tables/AccountsTable.cpp
+++ src/Wallet/WalletDB/Sqlite/Tables/AccountsTable.cpp
@@ -17,44 +17,11 @@ void AccountsTable::CreateTable(SqliteDB& database)
 
 void AccountsTable::UpdateSchema(SqliteDB& database, const int previousVersion)
 {
-	if (previousVersion < 3) {
+	if (previousVersion < 2) {
 		CreateTable(database);
-	} else if (previousVersion < 4) {
-		WALLET_INFO("Migrating Accounts Table to new schema");
-
-		// Create "new_outputs" table
-		std::string table_creation_cmd = "create table new_accounts(parent_path TEXT PRIMARY KEY, account_name TEXT NOT NULL, next_child_index INTEGER NOT NULL, current_address_index INTEGER NOT NULL);";
-		database.Execute(table_creation_cmd);
-
-		// Load existing slates
-		std::string get_slate_query = "SELECT parent_path, account_name, next_child_index FROM accounts";;
-		auto pStatement = database.Query(get_slate_query);
-
-		while (pStatement->Step()) {
-			WALLET_DEBUG_F(
-				"Migrating slate with id {} at stage {}",
-				pStatement->GetColumnString(0),
-				pStatement->GetColumnString(1)
-			);
-
-			std::string insert_slate_cmd = "insert into new_accounts values(?, ?, ?, ?)";
-
-			std::vector<SqliteDB::IParameter::UPtr> parameters;
-			parameters.push_back(TextParameter::New(pStatement->GetColumnString(0)));
-			parameters.push_back(TextParameter::New(pStatement->GetColumnString(1)));
-			parameters.push_back(IntParameter::New(pStatement->GetColumnInt(2)));
-			parameters.push_back(IntParameter::New(0));
-
-			database.Update(insert_slate_cmd, parameters);
-		}
-
-		// Delete existing table
-		std::string drop_table_cmd = "DROP TABLE accounts";
-		database.Execute(drop_table_cmd);
-
-		// Rename "new_accounts" table to "slate"
-		const std::string rename_table_cmd = "ALTER TABLE new_accounts RENAME TO accounts";
-		database.Execute(rename_table_cmd);
+	} else if (previousVersion < 3) {
+		// Nothing yet
+		// WALLET_INFO("Migrating Accounts Table to new schema");
 	}
 }
 

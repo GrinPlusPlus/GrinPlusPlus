2b5bc871ce3e5e21ca832234fc0b7e0e20bb0b8f
David Tavarez <337107+davidtavarez@users.noreply.github.com>
2022-09-13 22:20:31 +0200

    adding bridge fingerprint

--- src/Core/Config/TorConfig.h
+++ src/Core/Config/TorConfig.h
@@ -202,7 +202,7 @@ private:
 			newConfigFile << "-url https://snowflake-broker.torproject.net.global.prod.fastly.net/ ";
 			newConfigFile << "-front cdn.sstatic.net ";
 			newConfigFile << "-ice stun:stun.l.google.com:19302,stun:stun.voip.blackberry.com:3478,stun:stun.altar.com.pl:3478,stun:stun.antisip.com:3478,stun:stun.bluesip.net:3478,stun:stun.dus.net:3478,stun:stun.epygi.com:3478,stun:stun.sonetel.com:3478,stun:stun.sonetel.net:3478,stun:stun.stunprotocol.org:3478,stun:stun.uls.co.za:3478,stun:stun.voipgate.com:3478,stun:stun.voys.nl:3478\n";
-			newConfigFile << "Bridge snowflake 192.0.2.3:1\n";
+			newConfigFile << "Bridge snowflake 192.0.2.3:1 2B280B23E1107BB62ABFC40DDCC8824814F80A72\n";
 		}
 		else
 		{

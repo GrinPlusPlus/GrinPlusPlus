d23477157dffa5710341bf358d7a72b198612f2a
David Tavarez <337107+davidtavarez@users.noreply.github.com>
2023-05-17 22:36:40 +0200

    enabling filter to get_peers

--- src/API/Node/Handlers/GetPeersHandler.h
+++ src/API/Node/Handlers/GetPeersHandler.h
@@ -15,12 +15,49 @@ public:
 
 	RPC::Response Handle(const RPC::Request& request) const final
 	{
-		const std::vector<PeerConstPtr> peers = m_pP2PServer->GetAllPeers();
+		if (!request.GetParams().has_value()) {
+			return request.BuildError(RPC::Errors::PARAMS_MISSING);
+		}
+
+		const Json::Value params = request.GetParams().value();
+		if (!params.isArray()) {
+			return request.BuildError("INVALID_PARAMS", "Expected array");
+		}
+		
+		std::vector<Json::Value> values_json;
+		for (auto iter = params.begin(); iter != params.end(); iter++)
+		{
+			values_json.push_back(*iter);
+		}
 
 		Json::Value json;
-		for (PeerConstPtr peer : peers)
+
+		if(values_json.size() == 0)
 		{
-			json.append(peer->ToJSON());
+			const std::vector<PeerConstPtr> peers = m_pP2PServer->GetAllPeers();
+			for (PeerConstPtr peer : peers)
+			{
+				json.append(peer->ToJSON());
+			}
+		}
+		else
+		{
+			std::unordered_set<IPAddress> peersWanted;
+
+			std::transform(
+				values_json.cbegin(), values_json.cend(),
+				std::inserter(peersWanted, peersWanted.end()),
+				[](const Json::Value& peer_json) {
+					std::optional<std::string> peer = JsonUtil::ConvertToStringOpt(peer_json);
+					return IPAddress::Parse(peer.value());
+				}
+			);
+			auto peer = *(peersWanted.begin());
+			std::optional<PeerConstPtr> peerOpt = m_pP2PServer->GetPeer(peer);
+			if (peerOpt.has_value())
+			{
+				json.append(peerOpt.value()->ToJSON());
+			}
 		}
 
 		Json::Value result;

0fde7d1aff901280e97c378397f0c060b6cccea6
David Tavarez <337107+davidtavarez@users.noreply.github.com>
2023-05-17 22:50:24 +0200

    ban_peer

--- src/API/Node/Handlers/BanPeerHandler.h
+++ src/API/Node/Handlers/BanPeerHandler.h
@@ -9,12 +9,55 @@
 class BanPeerHandler : public RPCMethod
 {
 public:
+	BanPeerHandler(const IP2PServerPtr& pP2PServer)
+		: m_pP2PServer(pP2PServer) { }
+	~BanPeerHandler() = default;
+
 	RPC::Response Handle(const RPC::Request& request) const final
 	{	
+		if (!request.GetParams().has_value()) {
+			return request.BuildError(RPC::Errors::PARAMS_MISSING);
+		}
+
+		const Json::Value params = request.GetParams().value();
+		if (!params.isArray()) {
+			return request.BuildError("INVALID_PARAMS", "Expected array");
+		}
+
+		std::vector<Json::Value> values_json;
+		for (auto iter = params.begin(); iter != params.end(); iter++)
+		{
+			values_json.push_back(*iter);
+		}
+		
+		if (values_json.size() == 0)
+		{
+			return request.BuildError("INVALID_PARAMS", "Empty array");
+		}
+		else
+		{
+			std::unordered_set<IPAddress> peersWanted;
+
+			std::transform(
+				values_json.cbegin(), values_json.cend(),
+				std::inserter(peersWanted, peersWanted.end()),
+				[](const Json::Value& peer_json) {
+					std::optional<std::string> peer = JsonUtil::ConvertToStringOpt(peer_json);
+					return IPAddress::Parse(peer.value());
+				}
+			);
+			auto peer = *(peersWanted.begin());
+			m_pP2PServer->BanPeer(peer, EBanReason::ManualBan);
+		}
+
 		Json::Value result;
 		result["Ok"] = "";
+
 		return request.BuildResult(result);
 	}
 
 	bool ContainsSecrets() const noexcept final { return false; }
+
+private:
+	IP2PServerPtr m_pP2PServer;
 };

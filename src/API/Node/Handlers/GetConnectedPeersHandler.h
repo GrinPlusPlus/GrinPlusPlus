5b80a7c0c2695ea9b9dda602129ce8dc2c88263d
David Tavarez <337107+davidtavarez@users.noreply.github.com>
2023-05-17 21:01:38 +0200

    get_connected_peers

--- src/API/Node/Handlers/GetConnectedPeersHandler.h
+++ src/API/Node/Handlers/GetConnectedPeersHandler.h
@@ -9,15 +9,28 @@
 class GetConnectedPeersHandler : public RPCMethod
 {
 public:
+	GetConnectedPeersHandler(const IP2PServerPtr& pP2PServer)
+		: m_pP2PServer(pP2PServer) { }
+	~GetConnectedPeersHandler() = default;
+
 	RPC::Response Handle(const RPC::Request& request) const final
 	{
+		const std::vector<PeerConstPtr> peers = m_pP2PServer->GetAllPeers();
+
+		Json::Value json;
+		for (PeerConstPtr peer : peers)
+		{
+			json.append(peer->ToJSON());
+		}
+
 		Json::Value result;
-		result["Ok"] = "";
+		result["Ok"] = json;
+
 		return request.BuildResult(result);
 	}
 
 	bool ContainsSecrets() const noexcept final { return false; }
 
 private:
-	IBlockChain::Ptr m_pBlockChain;
+	IP2PServerPtr m_pP2PServer;
 };
